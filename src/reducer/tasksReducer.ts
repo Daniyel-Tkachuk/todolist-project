efe857734d6052e61f91fad8bd73bc81f3321cdb
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-19 21:48:20 +0300

    refactor

--- src/reducer/tasksReducer.ts
+++ src/reducer/tasksReducer.ts
@@ -1,10 +1,10 @@
 import {TaskType} from "../components/Todolist";
 import {v1} from "uuid";
 
-export const tasksReducer = (state: TaskType[], action: ActionType) => {
+export const tasksReducer = (state: TaskType[], action: ActionsType): TaskType[] => {
    switch (action.type) {
       case "REMOVE_TASK": {
-         return state.map(t => t.id !== action.payload.id);
+         return state.filter(t => t.id !== action.payload.id);
       }
       case "ADD_TASK": {
          const newTask = {id: v1(), title: action.payload.title, isDone: false};
@@ -13,6 +13,10 @@ export const tasksReducer = (state: TaskType[], action: ActionType) => {
       case "CHANGE_TASK_STATUS": {
          const {id, isDone} = action.payload;
          return state.map(t => t.id === id ? {...t, isDone} : t);
+      }
+      case "UPDATE_TASK_TITLE": {
+         const {id, title} = action.payload;
+         return state.map(t => t.id === id ? {...t, title} : t);
       }
       default: {
          return state
@@ -40,7 +44,7 @@ export const AddTaskAC = (title: string) => {
    } as const;
 };
 
-type ChangeTaskStatusACType= ReturnType<typeof ChangeTaskStatusAC>;
+type ChangeTaskStatusACType = ReturnType<typeof ChangeTaskStatusAC>;
 export const ChangeTaskStatusAC = (id: string, isDone: boolean) => {
    return {
       type: "CHANGE_TASK_STATUS",
@@ -51,4 +55,16 @@ export const ChangeTaskStatusAC = (id: string, isDone: boolean) => {
    } as const;
 }
 
-type ActionType = RemoveTaskACType | AddTaskACType | ChangeTaskStatusACType;
+type updateTaskTitleACType = ReturnType<typeof updateTaskTitleAC>;
+export const updateTaskTitleAC = (id: string, title: string) => {
+   return {
+      type: "UPDATE_TASK_TITLE",
+      payload: {
+         id,
+         title
+      }
+   } as const;
+}
+
+type ActionsType = RemoveTaskACType | AddTaskACType | ChangeTaskStatusACType
+   | updateTaskTitleACType;
