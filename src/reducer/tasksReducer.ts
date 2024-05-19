72a52b4a02ea1ccbfd1634b80c7292035f0f79ef
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-19 21:06:04 +0300

    refactor

---
+++ src/reducer/tasksReducer.ts
@@ -0,0 +1,10 @@
+export const tasksReducer = (state: any, action: any) => {
+   switch (action.type) {
+      case "XXX": {
+         return ;
+      }
+      default: {
+         return state
+      }
+   }
+}
