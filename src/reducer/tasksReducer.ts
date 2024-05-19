2ca43a3afea177a5f1c4d7b1df423750436411b8
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-19 21:43:44 +0300

    refactor

--- src/reducer/tasksReducer.ts
+++ src/reducer/tasksReducer.ts
@@ -1,9 +1,18 @@
 import {TaskType} from "../components/Todolist";
+import {v1} from "uuid";
 
 export const tasksReducer = (state: TaskType[], action: ActionType) => {
    switch (action.type) {
       case "REMOVE_TASK": {
-         return state.map(t => t.id !== action.id);
+         return state.map(t => t.id !== action.payload.id);
+      }
+      case "ADD_TASK": {
+         const newTask = {id: v1(), title: action.payload.title, isDone: false};
+         return [newTask, ...state];
+      }
+      case "CHANGE_TASK_STATUS": {
+         const {id, isDone} = action.payload;
+         return state.map(t => t.id === id ? {...t, isDone} : t);
       }
       default: {
          return state
@@ -11,13 +20,35 @@ export const tasksReducer = (state: TaskType[], action: ActionType) => {
    }
 };
 
-type RemoveTaskType = ReturnType<typeof removeTask>;
-
-export const removeTask = (id: string) => {
+type RemoveTaskACType = ReturnType<typeof removeTaskAC>;
+export const removeTaskAC = (id: string) => {
    return {
       type: "REMOVE_TASK",
-      id
+      payload: {
+         id
+      }
    } as const;
 };
 
-type ActionType = RemoveTaskType;
+type AddTaskACType = ReturnType<typeof AddTaskAC>;
+export const AddTaskAC = (title: string) => {
+   return {
+      type: "ADD_TASK",
+      payload: {
+         title
+      }
+   } as const;
+};
+
+type ChangeTaskStatusACType= ReturnType<typeof ChangeTaskStatusAC>;
+export const ChangeTaskStatusAC = (id: string, isDone: boolean) => {
+   return {
+      type: "CHANGE_TASK_STATUS",
+      payload: {
+         id,
+         isDone
+      }
+   } as const;
+}
+
+type ActionType = RemoveTaskACType | AddTaskACType | ChangeTaskStatusACType;
