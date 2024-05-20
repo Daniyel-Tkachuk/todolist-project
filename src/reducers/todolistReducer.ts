45b62b542e7da8f5decc41fe454d18bc2ea6b013
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-05-20 22:56:39 +0300

    added todolistReducer.ts

---
+++ src/reducers/todolistReducer.ts
@@ -0,0 +1,13 @@
+export const todolistReducer = (state: any, action: ActionsType): any => {
+   switch (action.type) {
+      case "xxx": {
+         return state;
+      }
+      default: {
+         return state;
+      }
+   }
+}
+
+
+type ActionsType = any;
