73fac593c08b5041a8e968fb58f8e63e01d54099
Daniyel Tkachuk <danilaradyn7357899@gmail.com>
2024-06-09 18:30:24 +0300

    added redux and added store

---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+import {combineReducers, legacy_createStore} from "redux";
+import {todolistsReducer} from "../reducers/todolistsReducer";
+import {tasksReducer} from "../reducers/tasksReducer";
+
+const rootReducer = combineReducers({
+   todolists: todolistsReducer,
+   tasks: tasksReducer
+});
+
+
+export const store = legacy_createStore(rootReducer);
+
+export type AppRootStateType = ReturnType<typeof rootReducer>;
+
+// @ts-ignore
+window.store = store;
